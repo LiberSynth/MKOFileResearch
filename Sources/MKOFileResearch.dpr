b8c34f48cdf394eb2a6b474a2b7868ed43c1db09
VasilyevSM <Vasilev@tillypad.ru>
2025-08-18 19:05:19 +0300

    Библиотека 1

---
+++ Sources/MKOFileResearch.dpr
@@ -0,0 +1,52 @@
+library MKOFileResearch;
+
+uses
+  System.SysUtils,
+  System.IOUtils,
+  uInterfaces in '..\..\MKOCommon\TaskManager\uInterfaces.pas',
+  uConsts in 'uConsts.pas' {$R *.res};
+
+{$R *.res}
+
+procedure MKOTaskLibraryMarker;
+begin
+end;
+
+procedure InitLibrary(MKOTaskLibrary: IMKOTaskLibrary); safecall;
+begin
+
+  with MKOTaskLibrary do
+  begin
+
+    RegisterTask(
+
+        SC_MASK_FILE_SEARCH_NAME,
+        SC_MASK_FILE_SEARCH_CAPTION,
+        SC_MASK_FILE_SEARCH_DESCRIPTION
+
+    );
+
+    RegisterTask(
+
+        SC_FILE_CONTENT_RESEARCH_NAME,
+        SC_FILE_CONTENT_RESEARCH_CAPTION,
+        SC_FILE_CONTENT_RESEARCH_DESCRIPTION
+
+    );
+
+  end;
+
+end;
+
+procedure FinLibrary; safecall;
+begin
+end;
+
+exports
+
+  MKOTaskLibraryMarker,
+  InitLibrary,
+  FinLibrary;
+
+begin
+end.
---
+++ Sources/uConsts.pas
@@ -0,0 +1,17 @@
+unit uConsts;
+
+interface
+
+const
+
+  SC_MASK_FILE_SEARCH_NAME = 'MaskFileSearch';
+  SC_MASK_FILE_SEARCH_CAPTION = 'Поиск файлов по маске';
+  SC_MASK_FILE_SEARCH_DESCRIPTION = 'Функционал поиска списка файлов по маске и стартовой папке поиска.';
+
+  SC_FILE_CONTENT_RESEARCH_NAME = 'FileContentResearch';
+  SC_FILE_CONTENT_RESEARCH_CAPTION = 'Поиск строки в файле';
+  SC_FILE_CONTENT_RESEARCH_DESCRIPTION = 'Функционал поиска вхождений последовательности символов в файле.';
+
+implementation
+
+end.
